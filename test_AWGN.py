e9d110be05b321f2ff4a833f0cfdbb755a31bfa3
PangTongyao <pangtongyao@u.nus.edu>
2021-12-07 10:22:10 +0700

    second commit

--- test_AWGN.py
+++ test_AWGN.py
@@ -15,8 +15,10 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 parser = argparse.ArgumentParser(description="DnCNN_Test")
 parser.add_argument("--num_of_layers", type=int, default=17, help="Number of total layers")
 parser.add_argument("--logdir", type=str, default="logs", help='path of log files')
-parser.add_argument("--test_data", type=str, default='Set12', help='test on Set12 or Set68')
+parser.add_argument("--test_data", type=str, default='bsd68', help='test dataset')
 parser.add_argument("--test_noiseL", type=float, default=25, help='noise level used on test set')
+parser.add_argument("--alpha", type=float, default=0.5, help='R2R recorruption parameter')
+parser.add_argument("--training", type=str, default="R2R", help='R2R or N2C')
 opt = parser.parse_args()
 
 def normalize(data):
@@ -48,13 +50,32 @@ def main():
         # noisy image
         INoisy = ISource + noise
         ISource, INoisy = Variable(ISource.cuda()), Variable(INoisy.cuda())
-        with torch.no_grad(): # this can save much memory
-            Out = torch.clamp(INoisy-model(INoisy), 0., 1.)
+        if opt.training == 'R2R':
+            alpha = opt.alpha
+            out_test = None
+            aver_num = 50
+            eps = opt.test_noiseL/255.
+            for test_j in range(aver_num):
+                INoisy_pert = INoisy + alpha*eps*torch.FloatTensor(INoisy.size()).normal_(mean=0, std=1.).cuda()
+                with torch.no_grad():
+                    out_test_single = model(INoisy_pert)
+                if out_test  is None:
+                    out_test= out_test_single.detach()
+                else:
+                    out_test += out_test_single.detach()
+                del out_test_single
+                
+            out_test = torch.clamp(out_test/aver_num, 0., 1.)
+            psnr = batch_PSNR(out_test, ISource, 1.)
+            psnr_test += psnr
+        else:
+            with torch.no_grad(): # this can save much memory
+                Out = torch.clamp(model(INoisy), 0., 1.)
         ## if you are using older version of PyTorch, torch.no_grad() may not be supported
         # ISource, INoisy = Variable(ISource.cuda(),volatile=True), Variable(INoisy.cuda(),volatile=True)
         # Out = torch.clamp(INoisy-model(INoisy), 0., 1.)
-        psnr = batch_PSNR(Out, ISource, 1.)
-        psnr_test += psnr
+            psnr = batch_PSNR(Out, ISource, 1.)
+            psnr_test += psnr
         print("%s PSNR %f" % (f, psnr))
     psnr_test /= len(files_source)
     print("\nPSNR on test data %f" % psnr_test)
